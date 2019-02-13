bf1cebc1a6463ddd8949c5d6189c43f8718c4851
Lunarbaby <lunarbaby21@hotmail.com>
2019-02-13 13:00:07 -0500

    First commit

---
+++ index.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+result = requests.get("https://www.google.com/")
+# print(result.status_code)
+# print(result.headers)
+
+src = result.content
+soup = BeautifulSoup(src, 'html.parser')
+
+links = soup.find_all("a")
+# print(links)
+# print("\n")
+
+for link in links:
+    if "About" in link.text:
+        print(link)
+        print(link.attrs['href'])
